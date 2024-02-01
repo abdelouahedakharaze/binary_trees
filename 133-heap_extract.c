b07f219d4ac243413066471acaf137a0bac25f29
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2024-02-01 15:08:46 +0100

    133-heap_extract.c

---
+++ 133-heap_extract.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - converte un Binary Max Heap in un array ordinato di interi
+ *
+ * @root: puntatore doppio al nodo radice dell'heap
+ * Return: valore memorizzato nel nodo radice
+ */
+int heap_extract(heap_t **root)
+{
+    return (root);
+}
